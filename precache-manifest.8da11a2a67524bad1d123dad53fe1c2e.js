b23bc1690f50b87a43f31d843c44fa7d24def33c
mastudily <mastudily@yahoo.es>
2020-05-05 15:17:02 -0500

    Updates

---
+++ precache-manifest.8da11a2a67524bad1d123dad53fe1c2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37279d7cf43ecdbfe7ef531cff7c89b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f78bf5b6acb93ee9c494",
+    "url": "/monsters-rolodex/static/css/main.9488a7a4.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monsters-rolodex/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "f78bf5b6acb93ee9c494",
+    "url": "/monsters-rolodex/static/js/main.7525ee62.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
